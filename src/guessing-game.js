f4a4db8d7e3ebcf663cb60061ce9b4a371db506c
BuTaMuH4uK <butamuh4uk98@mail.ru>
2017-10-12 00:28:13 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {
+		this.min;
+		this.max;
+	}
 
-    setRange(min, max) {
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
+	}
 
-    }
+	guess() {
+		return Math.ceil((this.max + this.min) / 2);
+	}
 
-    guess() {
+	lower() {
+		this.max = Math.ceil((this.max + this.min) / 2);
+	}
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	greater() {
+		this.min = Math.ceil((this.max + this.min) / 2);
+	}
 }
 
 module.exports = GuessingGame;
